809e4895440554e7a9af93ddc872da5f920a1ae6
NguyenSongHoangHuy <49484648+NguyenSongHoangHuy@users.noreply.github.com>
2019-12-12 01:21:18 +0700

    update background repeat

--- FinalProject/Assets/Scripts/BackgroudLoop.cs
+++ FinalProject/Assets/Scripts/BackgroudLoop.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class BackgroudLoop : MonoBehaviour
 {
+    public float speed = 0.5f;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 offset = new Vector2(Time.time * speed, 0);
+        GetComponent<Renderer>().material.mainTextureOffset = offset;
+    }
+    private void AddNewBackground()
+    {
+    }
+    private void SwapBackground()
+    {
     }
 }
